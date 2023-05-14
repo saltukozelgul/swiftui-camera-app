38629c36fe827f60be7a7af27ee154f16b6d1937
saltukozelgul <saltukozelgul@gmail.com>
2023-05-14 21:50:06 +0300

    Now App can create a new video from captured frames from camera

--- OverlayProject/ViewController.swift
+++ OverlayProject/ViewController.swift
@@ -1,28 +1,38 @@
 import UIKit
 import SwiftUI
+import OpenGLES.ES2
 import AVFoundation
+import Photos
+
 
 class ViewController : UIViewController,AVCaptureVideoDataOutputSampleBufferDelegate {
     private var permissionGranted = false
     private let captureSession = AVCaptureSession()
     private let sessionQueue = DispatchQueue(label: "sessionQueue")
-    
+
     private var previewLayer = AVCaptureVideoPreviewLayer()
     var videoDataOutput: AVCaptureVideoDataOutput!
     var videoDataOutputQueue: DispatchQueue!
     var assetWriter: AVAssetWriter!
     var assetWriterInput: AVAssetWriterInput!
     var pixelBufferAdaptor: AVAssetWriterInputPixelBufferAdaptor!
-    let movieFileOutput = AVCaptureMovieFileOutput()
     var isRecording = false
+    var wasFirstBuffer = false
+    var videoOutputURL: URL!
     
-    
+    // create editedBuffers array type CGSampleBuffer which have 1500 frame size
+    var editedBuffers = [CMSampleBuffer]()
     
     var screenRect: CGRect! = nil
     let recordButton = UIButton(type: .system)
     
     override func  viewDidLoad() {
         checkPermission()
+        isRecording = false
+
+        // clear editedBuffers
+        editedBuffers.removeAll()
+        
             
         // Record Button
         recordButton.setTitle("Record", for: .normal)
@@ -39,6 +49,7 @@ class ViewController : UIViewController,AVCaptureVideoDataOutputSampleBufferDele
         sessionQueue.async { [unowned self] in
             guard permissionGranted else {return}
             self.setupCaptureSession()
+            self.setupAssetWriter()
             self.captureSession.startRunning()
         }
     }
@@ -93,14 +104,100 @@ class ViewController : UIViewController,AVCaptureVideoDataOutputSampleBufferDele
     }
     
     @objc func toggleRecording() {
-
+        isRecording.toggle()
+        if isRecording {
+            recordButton.setTitle("Stop", for: .normal)
+        } else {
+            recordButton.setTitle("Record", for: .normal)
+            finishAssetWriter()
+        }
     }
-
-    func captureOutput(_ captureOutput: AVCaptureOutput!, didOutput sampleBuffer: CMSampleBuffer!, from connection: AVCaptureConnection!) {
-        // Do something with the video frame.
-        print("Frame captured")
+    
+    func setupAssetWriter() {
+        // Create asset writer
+        let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+        // Create videoOutputURL type Url and if its already exits remove first
+        let videoOutputURL = URL(fileURLWithPath: documentsPath).appendingPathComponent("output.mov")
+        
+        // Remove file if already exits
+        do {
+            try FileManager.default.removeItem(at: videoOutputURL)
+        } catch {
+            print(error)
+        }
+        
+        do {
+            assetWriter = try AVAssetWriter(outputURL: videoOutputURL, fileType: AVFileType.mov)
+        } catch {
+            print(error)
+        }
+        
+        // Create asset writer input
+        let videoSettings = [
+            AVVideoCodecKey : AVVideoCodecType.h264,
+            AVVideoWidthKey : 1920,
+            AVVideoHeightKey : 1080
+        ] as [String : Any]
+        assetWriterInput = AVAssetWriterInput(mediaType: AVMediaType.video, outputSettings: videoSettings)
+        assetWriterInput.expectsMediaDataInRealTime = true
+        assetWriter.add(assetWriterInput)
+        
+        // Create pixel buffer adaptor
+        let sourcePixelBufferAttributesDictionary = [
+            kCVPixelBufferPixelFormatTypeKey as String : NSNumber(value: kCVPixelFormatType_32BGRA),
+            kCVPixelBufferWidthKey as String : NSNumber(value: 1920),
+            kCVPixelBufferHeightKey as String : NSNumber(value: 1080)
+        ]
+        pixelBufferAdaptor = AVAssetWriterInputPixelBufferAdaptor(assetWriterInput: assetWriterInput, sourcePixelBufferAttributes: sourcePixelBufferAttributesDictionary)
+        
+        assetWriter.startWriting()
+        print("AssetWriter init")
+        print(videoOutputURL)
         
     }
+    
+    func finishAssetWriter() {
+
+        
+        // Finish writing session
+        assetWriterInput.markAsFinished()
+        assetWriter.finishWriting {
+            print("Finished writing")
+        }
+        
+        // Save the video to the photo library on main thread
+        DispatchQueue.main.async { [self] in
+            UISaveVideoAtPathToSavedPhotosAlbum(assetWriter.outputURL.path, nil, nil, nil)
+        }
+        
+    }
+
+    func captureOutput(_ captureOutput: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        // Do something with the video frame.-
+        // Add the sample buffer to the global array.
+        if isRecording {
+            if wasFirstBuffer == false {
+                assetWriter.startSession(atSourceTime: CMSampleBufferGetPresentationTimeStamp(sampleBuffer))
+                wasFirstBuffer = true
+            }
+            
+            // Edit buffer and add text to it
+            // TODO - EDIT THIS BUFFER BY USING BITMAX PAINTING OR OPENGL
+            
+            
+            pixelBufferAdaptor.append(CMSampleBufferGetImageBuffer(sampleBuffer)!, withPresentationTime: CMSampleBufferGetPresentationTimeStamp(sampleBuffer))
+        }
+        print(sampleBuffer)
+        
+    
+    }
+
+    
+    func addBufferToListAsync (buffer: CMSampleBuffer) {
+        editedBuffers.append(buffer)
+    }
+    
+    
     
     func setupCaptureSession() {
         // Access camera
